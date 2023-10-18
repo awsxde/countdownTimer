182b666d295b262732cda2ef20e67da1a6c527d0
awsxde <ali23khour@gmail.com>
2023-10-19 03:16:09 +0330

    fixing not display css bug

--- src/App.js
+++ src/App.js
@@ -1,1 +1,58 @@
-export default function App() {}
+export default function App() {
+  return (
+    <div className="container">
+      <h1 className="title">We're Launching soon</h1>
+      <div className="timerContainer">
+        <div className="timerChild">
+          <div className="blackCircleLeft"></div>
+          <div className="blackCircleRight"></div>
+          <div className="timerChildTop">
+            <p>23</p>
+          </div>
+          <div className="timerChildBottom">
+            <p>23</p>
+          </div>
+          <p className="timerChildTitle">days</p>
+        </div>
+        <div className="timerChild">
+          <div className="blackCircleLeft"></div>
+          <div className="blackCircleRight"></div>
+          <div className="timerChildTop">
+            <p>23</p>
+          </div>
+          <div className="timerChildBottom">
+            <p>23</p>
+          </div>
+          <p className="timerChildTitle">hours</p>
+        </div>
+        <div className="timerChild">
+          <div className="blackCircleLeft"></div>
+          <div className="blackCircleRight"></div>
+          <div className="timerChildTop">
+            <p>23</p>
+          </div>
+          <div className="timerChildBottom">
+            <p>23</p>
+          </div>
+          <p className="timerChildTitle">minutes</p>
+        </div>
+        <div className="timerChild">
+          <div className="blackCircleLeft"></div>
+          <div className="blackCircleRight"></div>
+          <div className="timerChildTop">
+            <p>23</p>
+          </div>
+          <div className="timerChildBottom">
+            <p>23</p>
+          </div>
+          <p className="timerChildTitle">seconds</p>
+        </div>
+      </div>
+      <div className="logosContainer">
+        <img src="../src/assets/images/icon-facebook.svg" alt="logo" />
+        <img src="../src/assets/images/icon-pinterest.svg" alt="logo" />
+        <img src="../src/assets/images/icon-instagram.svg" alt="logo" />
+      </div>
+    </div>
+  );
+}
